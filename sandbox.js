35c25b5bf370c115736b481b65080a11198699fe
mirzapm13 <mirzapm13@gmail.com>
2022-08-17 22:40:44 +0700

    fix session id

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,9 @@
 const bcryptjs = require('bcryptjs')
 
 let salt = bcryptjs.genSaltSync(8);
-let hash = bcryptjs.hashSync("mirza123", salt);
+let hash = bcryptjs.hashSync("ajip12345", salt);
 
 // user.password = hash
 
 console.log(hash);
-console.log(bcryptjs.compareSync('mirza123',hash));
+console.log(bcryptjs.compareSync('ajip12345',hash));
