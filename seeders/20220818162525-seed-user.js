10146da563b590abf642a4fc118948005b4dc929
Aji Purnomo <semlakune@gmail.com>
2022-08-18 23:27:43 +0700

    add seed user

---
+++ seeders/20220818162525-seed-user.js
@@ -0,0 +1,34 @@
+'use strict';
+const fs = require('fs');
+
+module.exports = {
+    up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   const users = JSON.parse(fs.readFileSync('./data/users.json', 'utf-8'))
+      .map(u => {
+        return {
+          ...u,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      })
+   return queryInterface.bulkInsert('Users', users)
+  },
+
+    down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
